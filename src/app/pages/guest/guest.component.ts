75bb0484f29523123f0ca26fb172f333a1a7ea37
Bouzga <bouzgaoumnia@gmail.com>
2025-07-08 23:44:04 +0100

    Développement de la partie Horaire/Réservation

--- src/app/pages/guest/guest.component.ts
+++ src/app/pages/guest/guest.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common'; // ✅ à importer
 import { MatIcon } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-guest',
-  standalone: true, // ⚠️ probablement manquant !
-  imports: [CommonModule, MatIcon], // ✅ Ajoute CommonModule ici
+  standalone: true,
+  imports: [CommonModule, MatIcon, FormsModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './guest.component.html',
-  styleUrl: './guest.component.scss'
+  styleUrl: './guest.component.scss',
+   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }]
 })
 export class GuestComponent {
   currentSlide = 0;
@@ -15,13 +19,16 @@ export class GuestComponent {
   isMobileMenuOpen = false;
   today = new Date();
   selectedDate: Date | null = null;
-  
-horaires: string[] = [
-  '9h30', '10h00', '10h30', '11h00', '11h30',
-  '12h00', '12h30', '13h00', '13h30',
-  '14h00', '14h30', '15h00', '15h30',
-  '16h00', '16h30', '17h00', '17h30'
-];
+  selectedFormule: string = '';
+  selectedDuree: string = '';
+
+
+  horaires: string[] = [
+    '9h30', '10h00', '10h30', '11h00', '11h30',
+    '12h00', '12h30', '13h00', '13h30',
+    '14h00', '14h30', '15h00', '15h30',
+    '16h00', '16h30', '17h00', '17h30'
+  ];
 
   ngOnInit() {
     setInterval(() => {
