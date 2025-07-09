b21447424309ea1606752f51239e42f8fda182aa
Bouzga <bouzgaoumnia@gmail.com>
2025-07-09 16:28:18 +0100

    Sauvegarde avant mise à jour Angular

--- src/app/pages/guest/guest.component.ts
+++ src/app/pages/guest/guest.component.ts
@@ -2,16 +2,20 @@ import { Component, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common'; // ✅ à importer
 import { MatIcon } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCalendarModule } from '@angular/material/datepicker';
 @Component({
   selector: 'app-guest',
   standalone: true,
-  imports: [CommonModule, MatIcon, FormsModule, MatDatepickerModule, MatNativeDateModule],
+  imports: [CommonModule, MatIcon, FormsModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatIconModule, MatButtonModule, CommonModule, FormsModule, MatCalendarModule, MatNativeDateModule, MatButtonModule,],
   templateUrl: './guest.component.html',
   styleUrl: './guest.component.scss',
-   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }]
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }]
 })
 export class GuestComponent {
   currentSlide = 0;
@@ -30,11 +34,26 @@ export class GuestComponent {
     '16h00', '16h30', '17h00', '17h30'
   ];
 
+
   ngOnInit() {
     setInterval(() => {
       this.currentSlide = (this.currentSlide + 1) % this.heroSlides.length;
     }, 7000);
   }
+
+
+  customDateClass(date: Date): string {
+    if (!this.selectedDate) return '';
+
+    return date.getDate() === this.selectedDate.getDate() &&
+      date.getMonth() === this.selectedDate.getMonth() &&
+      date.getFullYear() === this.selectedDate.getFullYear()
+      ? 'custom-selected-date'
+      : '';
+  }
+
+
+
 
   heroSlides = [
     {
@@ -75,4 +94,19 @@ export class GuestComponent {
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
+
+  onDateSelected(date: Date) {
+  this.selectedDate = date;             // pas de clone !
+  console.log('Date sélectionnée :', this.selectedDate);
+}
+
+getDateClass = (d: Date): string => {
+  return this.selectedDate &&
+         d.getDate()   === this.selectedDate.getDate() &&
+         d.getMonth()  === this.selectedDate.getMonth() &&
+         d.getFullYear() === this.selectedDate.getFullYear()
+           ? 'custom-selected-date'
+           : '';
+};
+
 }
